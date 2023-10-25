d0f6951cfb2c67cc2277f3bee3fd776689f5c3ca
ngminhoang <108173409+ngminhoang@users.noreply.github.com>
2023-10-25 14:05:58 +0700

    update7

--- Solution/Repositories_Do_An/DBcontext_vs_Entities/Order.cs
+++ Solution/Repositories_Do_An/DBcontext_vs_Entities/Order.cs
@@ -30,12 +30,19 @@ namespace Repositories_Do_An.DBcontext_vs_Entities
         public DateTime OrderedDate { get; set; }
         public DateTime ArrivedDate { get; set; }
         public BigInteger TotalAmount { get; set; }
+
+
+
         [ForeignKey("Postion")]
         public int PostionComeId { get; set; }
         public virtual Position positionCome { get; set; }
+
+
         [ForeignKey("Postion")]
         public int PostionGoId { get; set; }
         public virtual Position positionGo { get; set; }
+
+
         public string paymentMethod { get; set; } = "Tài xế và khách hàng tự làm việc với nhau";
         public bool Status { get; set; }
     }
